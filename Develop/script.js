ae2c960104aaec756fcc66aae9353f527f4889dd
kflinnn <kflinncoding@gmail.com>
2022-11-26 10:55:46 -0500

    started function writing

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,47 @@
 //Create global variables for password entries
-var specials = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-console.log(specials)
-console.log(numbers)
-console.log(lowercase)
-console.log(uppercase)
+ var specials = ["!", "@", "#", "$", "%", "^", "&", "*"];
+ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// // // Assignment Code
+ var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+// // // Write password to the #password input
+ function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+   passwordText.value = password;
+ }
 
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// // // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //Presented with prompts for how many characters in password
 function generatePassword() {
-  var amountCharacters = window.prompt("Please select number of characters (must be between 8 and 128 characters)");
+  var amountCharacters = window.parseInt("Please select number of characters (must be between 8 and 128 characters)");
+  console.log(typeof amountCharacters)
+  console.log(amountCharacters)
 
+
+  // function lengthRange(inputtxt, minlength, maxlength)
+  // {  	
+  //    var userCharacterInput = inputtxt.value;  
+  //    if(userCharacterInput.length >= 8 && userInput.length <= 128)
+  //       {  	
+  //         return true;  	
+  //       }
+  //    else
+  //       {  	
+  //   alert("Please input between " + 8 + " and " + 128 + " characters");  		
+  //         return false;  	
+  //       }  
+  // }
 }
+generatePassword()
+//lengthRange()
+
 
 
 
